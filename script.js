9d3694a3f5f3f92c98f818e0b7e803cb13b9018c
hbmubin <hasanulbannamubin01@gmail.com>
2024-02-14 19:03:24 +0600

    dd

---
+++ script.js
@@ -0,0 +1,11 @@
+// function play() {
+//   const willHide = document.getElementById("home-screen");
+//   willHide.classList.add("hidden");
+//   const willShow = document.getElementById("ground-screen");
+//   willShow.classList.remove("hidden");
+// }
+
+function play() {
+  hideElementById("home-screen");
+  showElementById("ground-screen");
+}
