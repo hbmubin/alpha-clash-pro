e84a6b6806c16659775691fd631a954b956e4d24
hbmubin <hasanulbannamubin01@gmail.com>
2024-02-14 23:54:28 +0600

    finished

--- script.js
+++ script.js
@@ -4,8 +4,15 @@
 //   const willShow = document.getElementById("ground-screen");
 //   willShow.classList.remove("hidden");
 // }
+function continueGame() {
+  const alphabet = getRandomAlphabet();
+  const currentAlphabet = document.getElementById("display");
+  currentAlphabet.innerText = alphabet;
+  setBackgroundColorById(alphabet);
+}
 
 function play() {
   hideElementById("home-screen");
   showElementById("ground-screen");
+  continueGame();
 }
--- utility.js
+++ utility.js
@@ -6,3 +6,16 @@ function showElementById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.remove("hidden");
 }
+function setBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-400");
+}
+function getRandomAlphabet() {
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = alphabetString.split("");
+
+  const random = Math.random() * 25;
+  const randomNumber = Math.round(random);
+  const randomAlphabet = alphabets[randomNumber];
+  return randomAlphabet;
+}
